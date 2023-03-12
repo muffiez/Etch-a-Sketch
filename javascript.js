bb12b622ebd83f38d08cd4dff4c054c673c24a63
Bryan <118778494+muffiez@users.noreply.github.com>
2023-03-12 13:11:36 -0400

    Add erase button and rgb button

--- javascript.js
+++ javascript.js
@@ -1,47 +1,83 @@
+const DEFAULT_SIZE = 16;
+
 const grid = document.getElementById('grid')
-
-for(let i = 0; i < 256; i++) {
-    const div = document.createElement("div");
-        div.className = "boxes";
-    grid.appendChild(div);
-    div.addEventListener('mouseover', () => {
-        div.classList.add('mouse-over');
-        });
-}
-
 const gridSize = document.querySelector("#gridSize");
 const input = document.querySelector("#sizeSlider");
+const clearBtn = document.querySelector("#clear");
+const eraseBtn = document.querySelector("#erase");
 
+
+let currentColor = 'black'; 
+
+const mouseOverEvent = (e) => {
+    const cell = e.target;
+    switch (currentColor) {
+        case 'black':
+            cell.style.backgroundColor = 'black';
+            break;
+        case 'erase':
+            cell.style.backgroundColor = 'white';
+            break;
+        case 'rgb':
+            const r = Math.floor(Math.random()*256);
+            const g = Math.floor(Math.random()*256);
+            const b = Math.floor(Math.random()*256);
+            cell.style.backgroundColor = `rgb(${r},${g},${b})`;
+            break;
+        default:
+            throw new Error("color does not exist");
+    }
+}
+
+let setupGrid = (size) => {
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
+    
+    for(let i = 0; i < size*size; i++) {
+        const cell = document.createElement("div");
+        cell.className = "cells";
+        grid.appendChild(cell);
+        cell.addEventListener('mouseover', mouseOverEvent)
+    }
+}
+
+
+
+let clearGrid = () => {
+    grid.innerHTML = ''
+}
+let refresh = () => {
+    clearGrid();
+    setupGrid(size);
+}
+
+// const cells = document.querySelector("cells");
+// clearBtn.addEventListener('click', refresh());
 
 
 input.addEventListener("input", (e) => { 
-  let size = e.target.value;
-  console.log(size)
-  gridSize.innerHTML = `${size} x ${size}`;
-  clearGrid();
-  setupGrid(size);
-  for(let i = 0; i < size*size; i++) {
-    const div = document.createElement("div");
-        div.className = "boxes";
-    grid.appendChild(div);
-    div.addEventListener('mouseover', () => {
-        div.classList.add('mouse-over');
-        });
-  }
+    size = e.target.value;
+    gridSize.innerHTML = `${size} x ${size}`;
+    clearGrid();
+    setupGrid(size);
 });
 
+ 
 
 
-  function setupGrid(size) {
-    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
-  }  
 
-  function clearGrid() {
-    grid.innerHTML = ''
-  }
-  
+const buttons = document.querySelectorAll('button');
 
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        const colorSelection = e.target.id;
+        console.log(colorSelection);
+        currentColor = colorSelection;
+    });
+});
 
+window.onload = () => {
+    setupGrid(DEFAULT_SIZE);
+}
 
 
